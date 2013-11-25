46e279abfb28f4bb044ec8551060fd44a889b065
tudou527 <etosun@qq.com>
2013-11-25 15:38:39 +0800

    up

--- app/mt.js
+++ app/mt.js
@@ -19,7 +19,7 @@ var MT = {
 }
 
 var sc = document.createElement('script')
-sc.src = 'http://etosun.com/html/mt-tools/alert.js?t=1114';
+sc.src = 'http://etosun.com/html/mt-tools/alert.js?t=1125';
 $('body').append(sc);
 
 var local = window.location.href;
